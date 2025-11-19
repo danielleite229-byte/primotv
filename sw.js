d9252d95923af444a6427438b51716f5fcac9df8
danielleite229-byte <danielleite229@gmail.com>
2025-11-19 14:10:52 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,1 @@
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open('primotv-v1').then(c => c.addAll(['/','/index.html','/manifest.json','/channels.json','/icons/icon-192.png'])));
-});
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
-});
+self.addEventListener('install',e=>self.skipWaiting());
